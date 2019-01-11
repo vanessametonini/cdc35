a17ba90bd01203af8d7006e5c5b1f50216749a5f
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-01-11 14:34:07 -0200

    Cap06. express validator

--- casacodigo/routes/produtos.js
+++ casacodigo/routes/produtos.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
+
     app.get('/produtos', function (request, response, next) {
-        
+
         const conexao = app.infra.connectionFactory();
         const produtoDao = new app.infra.ProdutoDao(conexao);
 
@@ -24,13 +25,41 @@ module.exports = function(app) {
     });
 
     app.get('/produtos/form', function(request, response) {
-        response.render('produtos/form');
+        const livro = { titulo: '', preco: '', descricao: '' }
+        response.render('produtos/form', {livro});
     });
     
     app.post('/produtos', function(request, response){
+        
+        request.assert('titulo', 'Titulo deve ser preenchido').notEmpty();
+        request.assert('preco', 'Preço deve ser um número').isFloat();
+        
+        const erros = request.validationErrors();
+        const livro = request.body
+
+        if (erros) {
+            response.format({
+                html: function () {
+                    response
+                    .status(400)
+                    .render('produtos/form', 
+                        { 
+                            validationErrors: erros, 
+                            livro
+                        }
+                    );
+                }
+                ,
+                json: function () {
+                    response.status(400).json(erros);
+                }
+            })
+
+            return;
+        }
+
         const conexao = app.infra.connectionFactory();
         const produtoDao = new app.infra.ProdutoDao(conexao);
-        const livro = request.body;
 
         produtoDao.cadastrar(livro, () => {
             response.redirect('/produtos');
